10178f6771b84834108fa5b295f51e9ce3d5f6e6
Stefan Ekdahl <stefan.silverek@gmail.com>
2016-04-12 13:31:51 +0200

    Shrivel shrivels

---
+++ Assets/Scripts/ShrivelTrigger.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrivelTrigger : MonoBehaviour {
+
+    private bool shrink = false;
+    private bool undone = false;
+    private bool grow = false;
+
+    private float xOriginal;
+    private float zOriginal;
+
+    private float timer = 0;
+
+    public float resizeSpeed = 0.1f;
+    public float growthDelay = 3.0f;
+
+    public GameObject target1;
+
+    // Use this for initialization
+    void Start ()
+    {
+        xOriginal = target1.transform.localScale.x;
+        zOriginal = target1.transform.localScale.z;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (shrink == true)
+        {
+            if (target1.transform.localScale.x >= xOriginal/10)
+            {
+                Debug.Log("Fucking hell!");
+                target1.transform.localScale += new Vector3(-resizeSpeed, 0, 0);
+            }
+
+            if (target1.transform.localScale.z >= zOriginal / 10)
+            {
+                target1.transform.localScale += new Vector3(0, 0, -resizeSpeed);
+            }
+
+
+        }
+	
+        if (undone == true && target1.transform.localScale.x < xOriginal)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= growthDelay)
+            {
+                shrink = false;
+                grow = true;
+                timer = 0;
+                undone = false;
+            }
+        }
+
+        if (grow == true)
+        {
+            if (target1.transform.localScale.x < xOriginal)
+            {
+                target1.transform.localScale += new Vector3(resizeSpeed, 0, 0);
+                Debug.Log("Up");
+            }
+
+            if (target1.transform.localScale.z < zOriginal)
+            {
+                target1.transform.localScale += new Vector3(0, 0, resizeSpeed);
+            }
+
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            shrink = true;
+            undone = false;
+            grow = false;
+            Debug.Log("Dis shit work!");
+            timer = 0;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            undone = true;
+            
+        }
+    }
+}
