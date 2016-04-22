716198622a9ea376773bad27ab34ef8cd828d209
kuribohlv9 <kuribohlv9@gmail.com>
2016-04-22 12:44:19 +0200

    debug buttons

---
+++ Assets/Scripts/DebugButtons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Debug F1"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        if (Input.GetButtonDown("Debug F2"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+	}
+}
