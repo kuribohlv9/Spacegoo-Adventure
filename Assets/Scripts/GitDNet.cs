37633a1e1dc4b6b1979ab2c3fff9d40f59e90df6
Dee <lucina.dee@gmail.com>
2016-05-03 17:26:46 +0200

    GitD IS ALMOST DONE!!! TY SIMOOONNNNEEEE

--- Assets/Scripts/GitDNet.cs
+++ Assets/Scripts/GitDNet.cs
@@ -1,90 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GitDNet : MonoBehaviour
-{
-
-    public int Number;
-    public Color glowColor = new Vector4(0.569F, 1, 1, 1);
-
-    private Light glowLight;
-
-    private float timer = 0;
-    public float timerTarget = 0.0f;
-
-
-    // Use this for initialization
-    void Start()
-    {
-
-        glowLight = GetComponent<Light>();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void LightUp()
-    {
-
-        Renderer rend = GetComponentInChildren<Renderer>();
-
-        rend.material.shader = Shader.Find("Standard");         //This is magically always using the right shader, regardless of order of material. Might be based on mesh where the script is attached (I put it on hats)  http://answers.unity3d.com/questions/960607/how-to-material-in-emission-color-change-1.html#comment-994024 
-        rend.material.SetColor("_EmissionColor", glowColor);
-
-        if (!glowLight.isActiveAndEnabled)
-        {
-            glowLight.enabled = !glowLight.enabled;               //THIS IS THE LIGHT FROM THE SHROOM. NECESSARY. DO NOT KILL
-          
-        }
-        
-    }
-
-    public void LightOff()
-    {
-
-        Renderer rend = GetComponentInChildren<Renderer>();
-
-        rend.material.shader = Shader.Find("Standard");
-        rend.material.SetColor("_EmissionColor", Color.black);      //Making the emission black is FASTER than turning it off (according to forum guy)
-
-        glowLight.enabled = !glowLight.enabled;               //THIS IS THE LIGHT FROM THE SHROOM. NECESSARY. DO NOT KILL
-
-    }
-
-    void OnTriggerEnter(Collider col)                   //CHANGE THIS TO ONSTAY, GLOWS WHEN PLAYER MOVES ON IT for 1.5s
-    {
-        if (col.tag == "Player")
-        {
-            timer += Time.deltaTime;
-
-            if (timer > timerTarget)
-            {
-                
-                LightUp();
-            }
-
-        }
-    }
-
-    void OnTriggerExit(Collider col)
-    {
-        if (col.tag == "Player")
-        {
-            LightOff();
-        }
-    }
-}
-
-
-
-
-
-
-//using UnityEngine;
+﻿//using UnityEngine;
 //using System.Collections;
 
 //public class GitDNet : MonoBehaviour
@@ -121,11 +35,12 @@ public class GitDNet : MonoBehaviour
 //        rend.material.shader = Shader.Find("Standard");         //This is magically always using the right shader, regardless of order of material. Might be based on mesh where the script is attached (I put it on hats)  http://answers.unity3d.com/questions/960607/how-to-material-in-emission-color-change-1.html#comment-994024 
 //        rend.material.SetColor("_EmissionColor", glowColor);
 
-//         if (!glowLight.isActiveAndEnabled)
-        //{
-        //    glowLight.enabled = !glowLight.enabled;               //THIS IS THE LIGHT FROM THE SHROOM. NECESSARY. DO NOT KILL
+//        if (!glowLight.isActiveAndEnabled)
+//        {
+//            glowLight.enabled = !glowLight.enabled;               //THIS IS THE LIGHT FROM THE SHROOM. NECESSARY. DO NOT KILL
           
-        //}
+//        }
+        
 //    }
 
 //    public void LightOff()
@@ -140,13 +55,6 @@ public class GitDNet : MonoBehaviour
 
 //    }
 
-//    void OnAwake()
-//    {
-//        //This subscribes the function LightUp to the onglownet event
-//        EventSystem.onglownet += LightUp;       //NOTE! NOT AS A FUNCTION! it's actually a variable!
-
-//    }
-
 //    void OnTriggerEnter(Collider col)                   //CHANGE THIS TO ONSTAY, GLOWS WHEN PLAYER MOVES ON IT for 1.5s
 //    {
 //        if (col.tag == "Player")
@@ -155,8 +63,8 @@ public class GitDNet : MonoBehaviour
 
 //            if (timer > timerTarget)
 //            {
-//                EventSystem.ActivateGlowNet();
-//                //LightUp();
+                
+//                LightUp();
 //            }
 
 //        }
@@ -170,6 +78,117 @@ public class GitDNet : MonoBehaviour
 //        }
 //    }
 //}
+
+
+
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class GitDNet : MonoBehaviour
+{
+
+    public int Number;
+    public Color glowColor = new Vector4(0.569F, 1, 1, 1);
+
+    private Light glowLight;
+
+    private float timer = 0;
+    public float timerTarget = 0.0f;
+
+    private bool IsLit = false;
+
+    void OnEnable()
+    {
+        EventSystem.onglownet += LightUp;
+    }
+    void OnDisable()
+    {
+        EventSystem.onglownet -= LightUp;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+        glowLight = GetComponent<Light>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+
+            if (IsLit && timer < timerTarget)
+            {
+                timer += Time.deltaTime;
+            }
+            else if (IsLit && timer > timerTarget)
+            {
+                LightOff();
+                timer = 0;
+            }
+
+        }
+    }
+
+    public void LightUp()
+    {
+        IsLit = true;
+        Renderer rend = GetComponentInChildren<Renderer>();
+
+        rend.material.shader = Shader.Find("Standard");         //This is magically always using the right shader, regardless of order of material. Might be based on mesh where the script is attached (I put it on hats)  http://answers.unity3d.com/questions/960607/how-to-material-in-emission-color-change-1.html#comment-994024 
+        rend.material.SetColor("_EmissionColor", glowColor);
+
+        if (!glowLight.isActiveAndEnabled)
+        {
+            glowLight.enabled = !glowLight.enabled;               //THIS IS THE LIGHT FROM THE SHROOM. NECESSARY. DO NOT KILL
+
+        }
+    }
+
+    public void LightOff()
+    {
+        IsLit = false;
+        Renderer rend = GetComponentInChildren<Renderer>();
+
+        rend.material.shader = Shader.Find("Standard");
+        rend.material.SetColor("_EmissionColor", Color.black);      //Making the emission black is FASTER than turning it off (according to forum guy)
+
+        glowLight.enabled = !glowLight.enabled;               //THIS IS THE LIGHT FROM THE SHROOM. NECESSARY. DO NOT KILL
+
+    }
+
+
+
+    void OnTriggerEnter(Collider col)                   //CHANGE THIS TO ONSTAY, GLOWS WHEN PLAYER MOVES ON IT for 1.5s
+    {
+        if (col.tag == "Player")
+        {
+        Debug.Log("JOGN");
+            timer += Time.deltaTime;
+
+            if (timer > timerTarget)
+            {
+                //LightUp();
+            }
+
+                EventSystem.ActivateGlowNet();
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+           // LightOff();
+        }
+    }
+}
+
+
 ////EXAMPLE TIME
 
 ////If we have the center mushroom's on player neter function here
