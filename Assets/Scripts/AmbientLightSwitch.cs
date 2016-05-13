1442044481af33a9c8491c930d04c52d94a35893
kuribohlv9 <kuribohlv9@gmail.com>
2016-05-13 17:53:37 +0200

    Dark area is now completely fix. also music

--- Assets/Scripts/AmbientLightSwitch.cs
+++ Assets/Scripts/AmbientLightSwitch.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class AmbientLightSwitch : MonoBehaviour {
 
+    private bool InControlIsHere = false;
     private bool IsDark = false;
     public GameObject lightGameObject;
 
+    private float AreaambientIntensity;
+    private float AreareflectionIntensity;
+    private Color AreaambientEquatorColor;
+    private Color AreaambientGroundColor;
+    private Color AreaambientSkyColor;
 	// Use this for initialization
 	void Start () {
-	
+
+        AreaambientIntensity = RenderSettings.ambientIntensity;
+        AreareflectionIntensity = RenderSettings.reflectionIntensity;
+
+        AreaambientEquatorColor = RenderSettings.ambientEquatorColor;
+        AreaambientGroundColor = RenderSettings.ambientGroundColor;
+        AreaambientSkyColor = RenderSettings.ambientSkyColor;
 	}
 	
 	// Update is called once per frame
@@ -16,52 +28,66 @@ public class AmbientLightSwitch : MonoBehaviour {
 
 	}
 
+    void LateUpdate()
+    {
+        if(InControlIsHere == false && IsDark)
+        {
+            TurnBright();
+        }
+        else if(InControlIsHere && IsDark == false)
+        {
+            TurnDark();
+        }
+        InControlIsHere = false;
+    }
+
     void OnTriggerEnter(Collider col)
     {
         //Debug.Log("John");
         if(col.tag == "Player")
         {
-<<<<<<< HEAD
-            //Debug.Log("Cena");
-            RenderSettings.ambientIntensity = 0.0f;
-=======
-            Debug.Log("Cena");
-            RenderSettings.ambientIntensity = 0.33f;
-            RenderSettings.ambientEquatorColor = Color.black;
-            RenderSettings.ambientGroundColor = Color.black;
-            RenderSettings.ambientSkyColor = Color.black;
-            RenderSettings.reflectionIntensity = 0.038f;
-          
->>>>>>> refs/remotes/origin/Dee4
+            TurnDark();
         }
     }
     void OnTriggerStay(Collider col)
     {
         if(col.tag == "Player")
         {
-            if(col.GetComponentInParent<PlayerMachine>().InControl && IsDark == false)
-            {
-                IsDark = true;
+            if (col.GetComponentInParent<PlayerMachine>().InControl == false)
+                return;
 
-                RenderSettings.ambientIntensity = 0.33f;
-                RenderSettings.ambientEquatorColor = Color.black;
-                RenderSettings.ambientGroundColor = Color.black;
-                RenderSettings.ambientSkyColor = Color.black;
-                RenderSettings.reflectionIntensity = 0.038f;
-               
-            }
-            else if (col.GetComponentInParent<PlayerMachine>().InControl == false && IsDark)
-            {
-                IsDark = false;
-                RenderSettings.ambientIntensity = 1;
-            }
+            InControlIsHere = true;
         }
     }
     void OnTriggerExit(Collider col)
     {
         if (col.tag == "Player")
         {
-            RenderSettings.ambientIntensity = 1;
+            TurnBright();
         }
+    }
+    private void TurnDark()
+    {
+        RenderSettings.ambientIntensity = 0.33f;
+        RenderSettings.reflectionIntensity = 0.038f;
+
+        RenderSettings.ambientEquatorColor = Color.black;
+        RenderSettings.ambientGroundColor = Color.black;
+        RenderSettings.ambientSkyColor = Color.black;
+
+        IsDark = true;
+        lightGameObject.SetActive(false);
+    }
+    private void TurnBright()
+    {
+        RenderSettings.ambientIntensity = AreaambientIntensity;
+        RenderSettings.reflectionIntensity = AreareflectionIntensity;
+
+        RenderSettings.ambientEquatorColor = AreaambientEquatorColor;
+        RenderSettings.ambientGroundColor = AreaambientGroundColor;
+        RenderSettings.ambientSkyColor = AreaambientSkyColor;
+
+        IsDark = false;
+        lightGameObject.SetActive(true);
     }
 }
