42c48fb3e3ace26ebb481c61c09fa881b7a5012a
kuribohlv9 <kuribohlv9@gmail.com>
2016-05-15 22:02:34 +0200

    Multiplayer implemented

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -5,7 +5,7 @@ public class PlayerInputController : MonoBehaviour {
 
     public PlayerInput Current;
     public Vector3 moveinput;
-    public string PlayerNumber = "0";
+    public string PlayerNumber = "1";
 
 	// Use this for initialization
 	void Start () {
@@ -15,26 +15,32 @@ public class PlayerInputController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
+
         // Retrieve our current WASD or Arrow Key input
         // Using GetAxisRaw removes any kind of gravity or filtering being applied to the input
         // Ensuring that we are getting either -1, 0 or 1   
         //Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Vector3 moveInput = new Vector3(Input.GetAxis("Left Stick X"), 0, Input.GetAxis("Left Stick Y"));
+        Vector3 moveInput = new Vector3(Input.GetAxis("Left Stick X" + PlayerNumber), 0, Input.GetAxis("Left Stick Y" + PlayerNumber));
         moveinput = moveInput;
-        Vector2 mouseInput = new Vector2(Input.GetAxis("Right Stick X"), Input.GetAxis("Right Stick Y"));
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Right Stick X" + PlayerNumber), Input.GetAxis("Right Stick Y" + PlayerNumber));
 
         if(mouseInput.magnitude < 0.01f)
         {
             mouseInput = Vector2.zero;
         }
 
-        bool jumpInput = Input.GetButtonDown("A Button");
-        bool continuousJumpInput = Input.GetButton("A Button");
-        bool sticky = Input.GetButtonDown("X Button");
-        bool debug = Input.GetButton("B Button");
-        bool recall = Input.GetButtonDown("Y Button");
-        bool leftbumper = Input.GetButtonDown("Swap Left");
-        bool rightbumper = Input.GetButtonDown("Swap Right");
+        bool jumpInput = Input.GetButtonDown("A Button" + PlayerNumber);
+        bool continuousJumpInput = Input.GetButton("A Button" + PlayerNumber);
+        
+        if(jumpInput == true)
+        {
+            int derp = 42;
+        }
+        bool sticky = Input.GetButtonDown("X Button" + PlayerNumber);
+        bool debug = Input.GetButton("B Button" + PlayerNumber);
+        bool recall = Input.GetButtonDown("Y Button" + PlayerNumber);
+        bool leftbumper = Input.GetButtonDown("Swap Left" + PlayerNumber);
+        bool rightbumper = Input.GetButtonDown("Swap Right" + PlayerNumber);
 
         Current = new PlayerInput()
         {
