e4197e63f49777e39d2a6f20f7769dc1b59cb55f
kuribohlv9 <kuribohlv9@gmail.com>
2016-05-20 18:27:55 +0200

    Startmenu fix and animation fix

--- Assets/Scripts/Menu/ControlsOption.cs
+++ Assets/Scripts/Menu/ControlsOption.cs
@@ -3,13 +3,4 @@ using System.Collections;
 
 public class ControlsOption : MenuOption {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        GetComponentInParent<StartMenu>().SetSuperLock(true);
-	}
 }
