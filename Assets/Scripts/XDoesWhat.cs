ebb66986a3ce04b4f704bc4414e392d7b3839ca6
Stefan Ekdahl <stefan.silverek@gmail.com>
2016-05-20 14:04:59 +0200

    What X does script

---
+++ Assets/Scripts/XDoesWhat.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class XDoesWhat : MonoBehaviour {
+
+    public Transform Sticky;
+    public Text output;
+
+    private int which = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (InsertPlayerInSlime(Sticky) && which != 1)
+        {
+            output.text = "Stick";
+            which = 1;
+        }
+
+        if (InsertPlayerInSlime(Sticky.GetComponent<PlayerMachine>().rightSwitchTarget) && which != 2)
+        {
+            output.text = "Glide";
+            which = 2;
+        }
+
+        if (InsertPlayerInSlime(Sticky.GetComponent<PlayerMachine>().rightSwitchTarget.GetComponent<PlayerMachine>().rightSwitchTarget) && which != 3)
+        {
+            output.text = "Power Jump";
+            which = 3;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private bool InsertPlayerInSlime(Transform target)
+    {
+        if (target.GetComponent<PlayerMachine>().InControl)
+        {
+            return true;
+        }
+        return false;
+    }
+}
