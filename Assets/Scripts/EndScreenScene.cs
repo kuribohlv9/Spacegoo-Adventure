f341ffe4b80f4769b908304a48c73227131b431c
kuribohlv9 <kuribohlv9@gmail.com>
2016-05-20 15:04:03 +0200

    Updated winning screen and time in startmenu

--- Assets/Scripts/EndScreenScene.cs
+++ Assets/Scripts/EndScreenScene.cs
@@ -7,9 +7,10 @@ public class EndScreenScene : MonoBehaviour {
     public string button;
     public Text instruction;
     public bool startfade = false;
+    public float looptime = 1f;
 
     private bool complete = false;
-    private float timer = 3;
+    private float timer = 3.2f;
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,6 +22,7 @@ public class EndScreenScene : MonoBehaviour {
                 complete = true;
                 instruction.enabled = true;
                 startfade = false;
+                timer = looptime;
             }
         }
         if(complete)
@@ -29,6 +31,19 @@ public class EndScreenScene : MonoBehaviour {
             {
                 UnityEngine.SceneManagement.SceneManager.LoadScene(0);
             }
+
+            //timer -= Time.deltaTime;
+            //if(timer < 0)
+            //{
+            //    Color temp = instruction.color;
+            //    if (temp.a == 0)
+            //        temp.a = 1;
+            //    else if (temp.a == 1)
+            //        temp.a = 0;
+
+            //    instruction.color = temp;
+            //    timer = looptime;
+            //}
         }
 	}
 }
