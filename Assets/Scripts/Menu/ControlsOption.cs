df9a638e4c6643d9476eb69fbf0c383d71e3543f
kuribohlv9 <kuribohlv9@gmail.com>
2016-05-20 19:18:08 +0200

    added final touches

--- Assets/Scripts/Menu/ControlsOption.cs
+++ Assets/Scripts/Menu/ControlsOption.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ControlsOption : MenuOption {
 
+    public Image controlimage;
+
+    void OnDisable()
+    {
+        controlimage.gameObject.SetActive(false);
+    }
+
+    public override void ExecuteOption()
+    {
+        GetComponentInParent<StartMenu>().SetSuperLock(true);
+        controlimage.gameObject.SetActive(true);
+    }
 }
